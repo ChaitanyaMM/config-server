9b8065a67949e62f59a8d2ef11e2584424901f50
ChaitanyaMM <krishnachaitu.17@gmail.com>
2021-02-07 14:42:29 +0530

    draft config-server

---
+++ src/main/java/com/practice/configserver/ConfigServerApplication.java
@@ -0,0 +1,24 @@
+package com.practice.configserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@EnableConfigServer
+@RestController
+public class ConfigServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConfigServerApplication.class, args);
+	}
+
+	@GetMapping("/")
+	public String sample() {
+
+		return "Config-server";
+	}
+
+}
